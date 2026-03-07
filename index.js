f08eee73d9059011bde301027f325bffed443088
HeartofTheForesttt <elyssastrawn8@gmail.com>
2026-03-07 00:32:06 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 const { Client, GatewayIntentBits } = require('discord.js');
 
 const client = new Client({
-  intents: [GatewayIntentBits.Guilds]
+  intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent],
 });
 
 client.once('ready', () => {
-  console.log(`Logged in as ${client.user.tag}`);
+  console.log(`🌲 Heart of the Forest awakens...`);
+  client.user.setActivity("watching the forest grow 🌱");
+});
+
+client.on('messageCreate', message => {
+  if (message.content === '!sprite') {
+    message.reply('🧚‍♀️ Sprite magic flickers through the trees...');
+  }
 });
 
 client.login(process.env.DISCORD_TOKEN);
